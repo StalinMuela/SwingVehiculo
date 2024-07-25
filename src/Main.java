270018f2154a5ab145f2b427aac6275cc277f8b5
StalinMuela <stalinsangucho87@gmail.com>
2024-07-25 07:25:27 -0500

    s

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.pack();
+        System.out.println("Hola");
         frame.setVisible(true);
 
     }
